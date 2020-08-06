dc856003f9ff133f0a62c005fdab0a11d08b61e8
Akash52 <ac8572611@gmail.com>
2020-08-06 18:34:48 +0530

    Pattern c++

---
+++ C++ Pattern Program/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for(int i=1;i<=4;i++)
+{
+	for(int j=1;j<=5;j++)
+	{
+		cout<<j;
+	}
+	cout<<"\n";
+}
+}
---
+++ C++ Pattern Program/4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for(int i=1;i<=4;i++)
+{
+	for(int j=1;j<=5;j++)
+	{
+		cout<<char j+64;
+	}
+	cout<<"\n";
+} 
+}
