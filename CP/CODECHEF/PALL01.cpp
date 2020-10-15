5ec81b9edcf24c3faf22ace5a4759f4370637c27
Akash52 <ac8572611@gmail.com>
2020-10-15 09:36:58 +0530

    bit magic

---
+++ CP/CODECHEF/PALL01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		string num=to_string(n);
+		cout<<num;
+	}
+	return 0;
+}
---
+++ DSA/Bit Magic/1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x=3,y=6;
+	cout<<(x&y)<<endl;
+	cout<<(x|y)<<endl;
+	cout<<(x^y)<<endl;
+	return 0;
+}
