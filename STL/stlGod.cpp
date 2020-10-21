f4586719f846da1e6fadc3066abe3ac56e1a3865
Akash52 <ac8572611@gmail.com>
2020-10-21 21:44:49 +0530

    storke text done

---
+++ STL/stlGod.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> A = {12, 23, 45, 13};
+    cout << A[1] << endl;
+    sort(A.begin(), A.end());
+    //12 13 23 45
+    //o(LogN)
+
+    bool present = binary_search(A.begin(), A.end(), 3); //true
+}
