a832ad649f5a8d467691ab0980270e6a948501b1
Akash52 <ac8572611@gmail.com>
2020-10-23 07:38:57 +0530

    Math

---
+++ DSA/Math/Fact_recu_ite.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int FactorialRecurision(int n)
+{
+	if(n==0)
+	return 1;
+	for(int i=0;i<n;i++)
+	return n*FactorialRecurision(n-1);
+}
+
+int FactorialIteration(int n)
+{
+	int res=1;
+	for(int i=2;i<n;i++)
+	res*=i;
+	return res;
+}
+
+int main()
+{
+	int n=5;
+	
+	cout<<"Factorial Recurision "<<FactorialRecurision(n)<<endl;
+	cout<<"Factorial Iteration "<<FactorialRecurision(n);
+	
+}
