89802eacb479ddb0a550abec40060ab495ff9237
Akash52 <ac8572611@gmail.com>
2020-10-26 07:17:23 +0530

    Array

---
+++ DSA/Array/5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void ArrayRever(int arr[],int start,int end)
+{
+	while(start<end)
+	{
+		int temp=arr[start];
+		arr[start]=arr[end];
+		arr[end]=temp;
+		start++;
+		end--;
+	}
+	
+}
+	void PrintArray(int arr[],int size)
+	{
+		for(int i=0;i<size;i++)
+		cout<<arr[i]<<" ";
+		cout<<endl;
+	}
+
+int main()
+{
+	int arr[]={1,2,3,4};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	ArrayRever(arr, 0, n-1);
+	cout << "Reversed array is" << endl;
+    PrintArray(arr, n);
+}
