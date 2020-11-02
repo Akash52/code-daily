800b8c35da565a9e538ec86f02363e8b1eb42369
Akash52 <ac8572611@gmail.com>
2020-11-02 11:23:33 +0530

    STL PAIR

---
+++ STL/Pair/pari_initi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main()
+{
+    pair<string, double> P1("GFG", 10.1212);
+    cout << P1.first << " ";
+    cout << P1.second << " ";
+    return 0;
+}
