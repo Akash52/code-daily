c214857c78fa257f4e2d6610adda4b0aa853caf8
Akash52 <ac8572611@gmail.com>
2020-11-15 16:59:06 +0530

    Recurison Pwr

---
+++ DSA/Recursion/2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int power(int a,int b)
+{
+	if(b==0)
+	return 1;
+	
+	return a*power(a,b-1);
+}
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	
+	cout<<power(a,b)<<endl;
+	
+	return 0;
+}
