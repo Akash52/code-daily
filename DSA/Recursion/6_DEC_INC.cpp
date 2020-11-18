95ee83a79c381274f8e3eb6e1ab19d73231d4348
Akash52 <ac8572611@gmail.com>
2020-11-18 09:37:43 +0530

    CPP RECURSION

---
+++ DSA/Recursion/6_DEC_INC.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dec(int n){
+	if(n==0)
+	return;
+	cout<<n<<" ";
+	dec(n-1);
+}
+void inc(int n)
+{
+	if(n==0)
+	return;
+	inc(n-1);
+	cout<<n<<" ";
+}
+
+int main()
+{
+	int n=5;
+	dec(n);
+	cout<<endl;
+	inc(n);
+	return 0;
+}
