3455dd0eea30bdcc60ae75f840caaec6cdf3774c
Akash52 <ac8572611@gmail.com>
2020-11-18 11:15:39 +0530

    CPP RECURSION

---
+++ DSA/Recursion/8_LIN_SEAR.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int *arr,int i,int n,int key){
+	if(i==n){
+		return -1;
+	}
+	if(arr[i]==key){
+		return i;
+	}
+	return linearSearch(arr,i+1,n,key);
+}
+
+
+
+int main(){
+	int arr[]={4,5,6,5};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int key=5;
+	cout<<"Found at index "<<linearSearch(arr,0,n,key);
+return 0;
+}
+	
+
