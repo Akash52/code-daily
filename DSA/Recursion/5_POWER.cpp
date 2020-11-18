de51a6941b401fff465078a99f901722886a38b6
Akash52 <ac8572611@gmail.com>
2020-11-18 09:29:02 +0530

    CPP RECURSION

---
+++ DSA/Recursion/5_POWER.cpp
@@ -0,0 +1,23 @@
+//Power
+#include<bits/stdc++.h>
+using namespace std;
+int power(int a,int b)
+{
+	if(b==0){
+	return 1;
+}
+else{
+	return a*power(a,b-1);
+}
+
+}
+
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<power(a,b)<<endl;
+	return 0;
+}
+
