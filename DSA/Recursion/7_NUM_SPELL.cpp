e2bc7feb05d8fad92c5bcef446e2945a530c8f9d
Akash52 <ac8572611@gmail.com>
2020-11-18 09:50:28 +0530

    CPP RECURSION

---
+++ DSA/Recursion/7_NUM_SPELL.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+char spellings[][10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+void print(int n){
+	if(n==0){
+		return;
+	}
+	print(n/10);
+	cout<<spellings[n%10]<<" ";
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	print(n);
+}
