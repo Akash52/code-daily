f58c4f1068adb5a89b6d7f971ccf3313b0dac4e0
Akash52 <ac8572611@gmail.com>
2020-12-23 11:32:34 +0530

    Array Move zero End

---
+++ DSA/Array/12.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void moveZero(int arr[],int n)
+{
+	int count=0;
+	for(int i=0;i<n;i++)
+	if(arr[i]!=0)
+			swap(arr[count++],arr[i]);
+			
+}
+void printArray(int arr[],int n){
+	for(int i=0;i<n;i++)
+	cout<<arr[i]<<" ";
+}
+
+int main()
+{
+	int arr[]={12,10,0,0,12,0};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	cout<<"Original Array : ";
+	printArray(arr,n);
+	moveZero(arr,n);
+	cout<<"\nModified Array : ";
+	printArray(arr,n);
+	
+	return 0;
+	
+	
+}
