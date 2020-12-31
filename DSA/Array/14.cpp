c22ca569fe65ced2fa2599c4706747f6893b59d4
Akash52 <ac8572611@gmail.com>
2020-12-31 21:19:56 +0530

    CPP

---
+++ DSA/Array/14.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void leftRotateArray(int arr[],int n)
+{
+	int temp=arr[0];
+	for(int i=1;i<n;i++)
+	arr[i]=temp;
+	arr[n-1]=temp;
+}
+
+void printArray(int arr[],int n)
+{
+	for(int i=0;i<n;i++)
+	cout<<arr[i]<<" ";
+}
+
+int main()
+{
+	
+	int arr[]={1,2,3,4,5,6};
+	int n=sizeof(arr)/sizeof(arr[0]);	
+	leftRotateArray(arr,n);
+	printArray(arr,n);
+	return 0;
+	
+}
