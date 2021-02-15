0fa9bc28d7281975dcfbb3e4be46ece2ae5d9247
Akash52 <ac8572611@gmail.com>
2021-02-15 19:29:39 +0530

    Pointer

---
+++ C++ Basic/Pointer/1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=10;
+	int *p;		// *p return actual value of A
+	p=&a;	//Adrees of a
+	cout<<p<<endl;  //Adress
+	cout<<*p<<endl;	//Value
+}
