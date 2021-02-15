e48aa803ccbb2c7e3714d2dd7b0b7d2991aabddc
Akash52 <ac8572611@gmail.com>
2021-02-15 19:42:32 +0530

    Pointer Arithmatic

---
+++ C++ Basic/Pointer/3.cpp
@@ -0,0 +1,16 @@
+//Pointer Arithmatic
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=10;
+	int *p;
+	p=&a;	
+	cout<<"Address of P is "<<p<<endl; 
+	cout<<sizeof(int)<<endl;
+	cout<<"Address at P+1 is "<<p+1<<endl;
+	cout<<sizeof(int)<<endl;
+	
+	
+}
