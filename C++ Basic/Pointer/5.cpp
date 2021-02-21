dcedd754f9eafaac659abd5bedaf2e1a439cb107
Akash52 <ac8572611@gmail.com>
2021-02-21 14:59:52 +0530

    Arrays and pointer

--- C++ Basic/Pointer/5.cpp
+++ C++ Basic/Pointer/5.cpp
@@ -14,4 +14,28 @@ int main()
 	cout<<*a<<endl; //a[0] print value
 	cout<<*(a+1)<<endl; //a[1] return 10
 	
+	int *p =&a[0];
+	
+	cout<<p<<endl; //Address
+	cout<<a<<endl;	//Address
+	
+	cout<<"Pointer P Address Changed : "<<&p<<endl;	// Adress chanaged
+	cout<<&a<<endl;	// Address Same
+	
+	cout<<sizeof(p)<<endl;
+	cout<<sizeof(a)<<endl;
+	
+	p=p+1;
+	//a=a+1; <- we can not assign like these
+	p=a+1; // these one is valid
+	cout<<p<<endl;
+	cout<<p<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
 }
