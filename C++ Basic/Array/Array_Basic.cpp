3bc4c664a5d666fd7d957c0f3c1e157c43f9c687
Akash52 <ac8572611@gmail.com>
2021-02-22 13:01:44 +0530

    Array Basic

---
+++ C++ Basic/Array/Array_Basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//Initializaation of an Array
+	int arr[10]={1,2,3};
+	
+	//Iterating an Array
+	for(int i=0;i<10;i++){
+		cout<<arr[i]<<",";
+	}
+}
