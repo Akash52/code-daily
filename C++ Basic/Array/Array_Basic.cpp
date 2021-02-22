f94ae718ebd50443ff147c76108cb6e137c74b2c
Akash52 <ac8572611@gmail.com>
2021-02-22 13:08:48 +0530

    Array Basic done

--- C++ Basic/Array/Array_Basic.cpp
+++ C++ Basic/Array/Array_Basic.cpp
@@ -4,10 +4,22 @@ using namespace std;
 
 int main()
 {
-	//Initializaation of an Array
+	//Initialization of an Array
 	int arr[10]={1,2,3};
 	
-	//Iterating an Array
+	//Size of
+	cout<<sizeof(arr)<<endl;
+	
+	//How many element in Array
+	int n=sizeof(arr)/sizeof(arr[0]);
+	cout<<n<<endl;	
+	
+	//Input all the element from the user
+	for(int i=0;i<5;i++){
+		cin>>arr[i];
+	}
+	
+	//Print all first 10 element
 	for(int i=0;i<10;i++){
 		cout<<arr[i]<<",";
 	}
