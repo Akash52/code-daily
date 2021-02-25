177330356e266406ad4cacc53d41cf23a7f04e65
Akash52 <ac8572611@gmail.com>
2021-02-25 20:00:51 +0530

    function Pointer

--- C++ Basic/Pointer/fun_ptr.cpp
+++ C++ Basic/Pointer/fun_ptr.cpp
@@ -5,9 +5,16 @@ void print(int *p){
 	cout<<*p<<endl;
 }
 
+void incrementPointer(int *p){
+	p=p+1;
+}
+
 int main()
 {
 	int i=10;
 	int *p=&i;
 	print(p);
+	cout<<p<<endl;
+	incrementPointer(p);
+	cout<<p<<endl;
 }
