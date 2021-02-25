5d7a31061bb8fde9220f415b997b1a665f73e9f1
Akash52 <ac8572611@gmail.com>
2021-02-25 15:31:28 +0530

    Character Pointer

--- C++ Basic/Pointer/char_ptr.cpp
+++ C++ Basic/Pointer/char_ptr.cpp
@@ -9,4 +9,13 @@ int main()
 	char b[]="abcd";
 	cout<<arr<<endl;
 	cout<<b<<endl;  // Return hall string
+	
+	char *c =&b[0];
+	cout<<c<<endl;
+	
+	char c1='a';
+	char *pc=&c1;
+	
+	cout<<c1<<endl;
+	cout<<pc<<endl;
 }
