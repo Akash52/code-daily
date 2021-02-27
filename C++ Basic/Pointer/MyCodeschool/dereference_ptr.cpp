f8719a4f86e94263864abb1f9a1acd0fb1c53152
Akash52 <ac8572611@gmail.com>
2021-02-27 14:18:47 +0530

    pointer dereferencing

--- C++ Basic/Pointer/MyCodeschool/dereference_ptr.cpp
+++ C++ Basic/Pointer/MyCodeschool/dereference_ptr.cpp
@@ -9,7 +9,7 @@
  	int *p;
  	a=10;
  	p=&a; //&a = address of a
- 	cout<<p<<endl;
- 	cout<<*p<<endl; // *p - value at adress pointed by p
- 	cout<<&a<<endl; // also return 
+ 	cout<<"a="<<a<<endl;
+ 	*p=12;  //dereferencing
+ 	cout<<"a="<<a<<endl;
  }
