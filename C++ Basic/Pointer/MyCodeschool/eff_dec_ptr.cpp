1ba13cbb1a4e24b02a282cad63ee13801458d67c
Akash52 <ac8572611@gmail.com>
2021-02-27 14:26:18 +0530

    pointer Arithmetic

---
+++ C++ Basic/Pointer/MyCodeschool/eff_dec_ptr.cpp
@@ -0,0 +1,10 @@
+ //Effecient Declaration of an Pointer
+ 
+ #include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int a;
+ 	int *p=&a;
+ }
---
+++ C++ Basic/Pointer/MyCodeschool/ptr_artithmetic.cpp
@@ -0,0 +1,14 @@
+ //Mycodeshool Reference
+ #include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int a;
+ 	int *p;
+ 	p=&a;
+ 	//Pointer Arithmetic
+ 	cout<<p<<endl;
+ 	cout<<p+1<<endl;
+ 	
+ }
