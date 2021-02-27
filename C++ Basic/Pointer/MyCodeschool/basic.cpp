d83db9ed63896acdd46bd81c9669234b97055e61
Akash52 <ac8572611@gmail.com>
2021-02-27 14:13:34 +0530

    basic of pointer

---
+++ C++ Basic/Pointer/MyCodeschool/basic.cpp
@@ -0,0 +1,14 @@
+ //Mycodeshool Reference
+ 
+ #include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int a;
+ 	int *p;
+ 	a=10;
+ 	p=&a; //&a = address of a
+ 	cout<<p<<endl;
+ 	cout<<*p<<endl; // *p - value at adress pointed by p
+ }
