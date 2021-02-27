e20690512396508baeeab06d83be3e787054f671
Akash52 <ac8572611@gmail.com>
2021-02-27 14:33:59 +0530

    pointer Arithmetic

--- C++ Basic/Pointer/MyCodeschool/ptr_artithmetic.cpp
+++ C++ Basic/Pointer/MyCodeschool/ptr_artithmetic.cpp
@@ -4,12 +4,14 @@
  
  int main()
  {
- 	int a;
+ 	int a=10;
  	int *p;
  	p=&a;
  	//Pointer Arithmetic
- 	cout<<"Address p is "<<p<<endl; //p is 2002
- 	cout<<"Size of integer "<<sizeof(int)<<" Bytes"<<endl;
- 	cout<<"Address p+1 is "<<p+1<<endl;	//p+1 is 2006
+ 	cout<<"\nAddress p is "<<p<<endl; //p is 2002
+ 	cout<<"\nvalue of p is "<<*p<<endl;
+ 	cout<<"\nSize of integer "<<sizeof(int)<<" Bytes"<<endl;
+ 	cout<<"\nAddress p+1 is "<<p+1<<endl;	//p+1 is 2006
+ 	cout<<"\nValue of p+1 is "<<(*p+1)<<endl;
  	
  }
