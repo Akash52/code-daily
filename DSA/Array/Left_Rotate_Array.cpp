65981be7558d90750d79528b17de1e9bde3c54d5
Akash52 <ac8572611@gmail.com>
2021-03-16 11:38:14 +0530

    Left Rotate by one Array

--- DSA/Array/Left_Rotate_Array.cpp
+++ DSA/Array/Left_Rotate_Array.cpp
@@ -6,7 +6,7 @@ void leftRotateArray(int arr[],int n)
 {
 	int temp=arr[0];
 	for(int i=1;i<n;i++)
-	arr[i]=temp;
+	arr[i-1]=arr[i];
 	arr[n-1]=temp;
 }
 
---
+++ DSA/Array/Practice/rotatebyone.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void leftRotateByOne(int arr[],int n){
+	int temp=arr[0];
+	for(int i=1;i<n;i++)
+	arr[i-1]=arr[i]; 
+	arr[n-1]=temp;
+	
+}
+
+void printArray(int arr[],int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+int main()
+{
+	int arr[]={1,2,3,4,5};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	leftRotateByOne(arr,n);
+	printArray(arr,n);
+	return 0;
+}
