58e5e32c2edafc70e64bb24b81d332379b8164ab
Akash52 <ac8572611@gmail.com>
2021-03-16 19:22:58 +0530

    Array Pointer Practice

---
+++ C++ Basic/Pointer/Practice Problems/12.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	//Assume integer takes 4 bytes and integer pointer 8 bytes.
+int a[5];
+int *c;
+cout << sizeof(a) << " "<< sizeof(c)<<endl;
+
+//Fill the output
+
+int b[] = {1, 2, 3, 4};
+cout << *(b) << " " << *(b+1)<<endl;
+
+//Assume that address of 0th index of array ‘a’ is : 200.
+
+int cr[6]={1,2,3};
+
+cout<<cr<<" "<<&cr<<endl;
+
+
+
+
+	
+	
+}
