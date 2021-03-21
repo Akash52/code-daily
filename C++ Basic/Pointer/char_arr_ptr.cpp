a64f210b8f0a6fdb739879fc3d3926542224991a
Akash52 <ac8572611@gmail.com>
2021-03-21 13:07:35 +0530

    charactre pointer array

---
+++ C++ Basic/Pointer/char_arr_ptr.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	
+	char str[]="abdce";
+	char *ptr="abcde";
+	
+	
+	
+	
+	//INT pointer charater 
+	int arr[]={1,2,3};
+	char b[]="abc";
+	cout<<arr<<endl;
+	cout<<b<<endl;
+	
+	char *c=&b[0];
+	cout<<c<<endl;
+	
+	char c1='A';
+	char *pc=&c1;
+	
+	cout<<c1<<endl;
+	cout<<pc<<endl;
+	
+	char sp[]= "hello";
+char *ps = sp;
+cout << sp[1] <<" "<< ps[0];
+
+	
+}
