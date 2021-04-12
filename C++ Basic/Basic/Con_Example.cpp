7af43915dbb0125a70bfe6c3f77527a8515d0748
Akash52 <ac8572611@gmail.com>
2021-04-12 15:43:20 +0530

    Practice

---
+++ C++ Basic/Basic/Con_Example.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Example{
+	int a,b;
+	public:
+		
+		Example(){
+			a=12;
+			b=12;
+		}
+		void Display()
+		{
+			cout<<a<<"\n"<<b<<endl;
+		}
+};
+
+
+int main()
+{
+	Example obj;
+	obj.Display();
+}
---
+++ C++ Basic/Basic/withot_mult_op_multiply.cpp
@@ -0,0 +1,18 @@
+//Withot arithmatic Operator Multiplication
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	int product=0;
+	cin>>a>>b;
+	
+	while(b!=0){
+		product+=a;
+		b--;
+	}
+	cout<<product<<endl;
+	return 0;
+	
+}
