ce7f49495c2a6c17f4d2ad1b0bea9bad63ded9b3
Akash52 <ac8572611@gmail.com>
2021-04-12 15:07:48 +0530

    Practice

---
+++ DSA/Array/Chracter Array/cin_getline.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	char a[100];
+	cin.getline(a,100,'$');
+	cout<<a<<endl;
+}
---
+++ DSA/Array/Chracter Array/palidrome.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isPalidrom(char a[]){
+	int i=0;
+	int j=strlen(a)-1;
+	while(i<j){
+		if(a[i]==a[j]){
+			i++;
+			j--;
+		}else{
+			return false;
+		}
+		return true;
+		
+	}
+}
+
+int main()
+
+{
+	char a[100];
+	cin.getline(a,100);
+	if(isPalidrom(a)){
+		cout<<"YES"<<endl;
+	}else{
+		cout<<"NO"<<endl;
+	}
+}
