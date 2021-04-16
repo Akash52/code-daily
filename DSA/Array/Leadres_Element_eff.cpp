f867c48ba003373db0fce57d1ab5128a8283513a
Akash52 <ac8572611@gmail.com>
2021-04-16 05:50:15 +0530

    Array Practice

---
+++ DSA/Array/Leadres_Element_eff.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void printLeader(int arr[],int n)
+{
+	int curr_ldr=arr[n-1];
+	cout<<curr_ldr<<" ";
+	for(int i=n-2;i>0;i--){
+		if(curr_ldr<arr[i]){
+			curr_ldr=arr[i];
+			cout<<curr_ldr<<" ";
+		}
+	}
+}
+
+
+int main(){
+	int arr[]={16, 17, 4, 3, 5, 2};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	printLeader(arr,n);
+	return 0;
+}
