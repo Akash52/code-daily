80f6a55bcb07e9ceb9c4435b040d61d0a2465c49
Akash52 <ac8572611@gmail.com>
2021-06-27 20:55:37 +0530

    practice some basic problem

---
+++ C++ Basic/Basic/Default_Function.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int a,int b,int c, int d=10,int e=10){
+	return (a+b+c+d+e);
+}
+
+int main()
+{
+	cout<<add(10,10,10);
+	return 0;
+}
---
+++ STL/vector/V1.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	for(int i=0;i<v.size();i++){
+		cout<<v[i]<<" ";
+	}
+}
