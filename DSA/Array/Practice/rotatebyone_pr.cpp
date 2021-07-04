a8db8f0e39322f28c0544548d9877380cd3cafc2
Akash52 <ac8572611@gmail.com>
2021-07-04 18:09:08 +0530

    practice array problems

---
+++ DSA/Array/Practice/rotatebyone_pr.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void leftRotate(int arr[],int n){
+	int temp=arr[0];
+	for(int i=1;i<n;i++)
+	arr[i-1]=arr[i];
+	arr[n-1]=temp;
+}
+
+void printArray(int arr[],int n){
+	for(int i=0;i<n;i++)
+	cout<<arr[i]<<" ";
+}
+
+int main()
+{
+	int arr[]={1,2,3,4,6};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	leftRotate(arr,n);
+	printArray(arr,n);
+	return 0; 
+}
